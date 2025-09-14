de1e3a961cbddbe459a714e57c98d9ae63428fea
bymjodfgad <bymjodfgad@gmail.com>
2025-09-14 22:13:47 +0200

    password checking if correct or false. and setting password and user

---
+++ OOP-ASSIGNMENT1/Inkapsling.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_ASSIGNMENT1
+{
+    class User
+    {
+        public string Username { get; private set; }  
+        private string password;                        
+
+        public User(string username) //sätter username
+        {
+            Username = username;
+        }
+
+        public void SetPassword(string pwd)//sätter password
+        {
+            password = pwd;
+        }
+
+        public bool VerifyPassword(string input)//kollar om password är samma som console inputen.
+        {
+            return password == input;
+        }
+    }
+}
