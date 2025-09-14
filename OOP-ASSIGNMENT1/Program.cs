5276bb7c4f1fccc89f562822a4795c2d6ddd636a
bymjodfgad <bymjodfgad@gmail.com>
2025-09-14 21:27:32 +0200

    Poly override

--- OOP-ASSIGNMENT1/Program.cs
+++ OOP-ASSIGNMENT1/Program.cs
@@ -4,11 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!(press enter)");
+            Console.WriteLine("Arv!= (press enter)");
             Console.ReadLine();
+
             Car theCar = new Car();
             theCar.Drive();
-            Console.ReadLine() ;    
+
+            Console.ReadLine() ;
+            Console.WriteLine("Polymorfism! (press enter)");
+            Console.ReadLine();
+            
+
+            //it just prints the overridden class instead of both
+            Shape circle = new Circle();
+            circle.Draw();
         }
     }
 }
